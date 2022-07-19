30dd0dc7d7140a9eb5746c4419bcdf7dafcdec32
Jorge Filho <jorge.filho@solides.com.br>
2022-07-19 18:05:46 -0300

    cenarios get list skills

--- __tests__/skills/list-skills-spec.js
+++ __tests__/skills/list-skills-spec.js
@@ -1,21 +1,19 @@
 import request from 'config/request';
 import client from 'helper/AuthClient';
-import groupCriteria from 'helper/GroupCriteria';
 import EXPIRED_TOKEN from 'utils/constants';
 import validate from 'helper/Validate';
-import successSchema from 'schemas/skill_groups/list/success';
-import expiredTokenSchema from 'schemas/skill_groups/list/expired_token';
+import successSchema from 'schemas/skills/list/success';
+import expiredTokenSchema from 'schemas/skills/list/expired_token';
 
-describe('List Group Criterias', () => {
+describe('List skills', () => {
   beforeAll(async () => {
     await client.auth();
-    await groupCriteria.create();
   });
 
   test('successfully', async () => {
     const res = await request
-      .get('skill_groups')
-      .set('Authorization', `Bearer ${client.accessToken}`);
+      .get('skills')
+      .set('Authorization', 'Bearer ' + client.accessToken);
 
     expect(res.headers).toHaveProperty(
       'content-type',
@@ -27,7 +25,7 @@ describe('List Group Criterias', () => {
 
   test('expired token', async () => {
     const res = await request
-      .get('skill_groups')
+      .get('skills')
       .set('Authorization', 'Bearer ' + EXPIRED_TOKEN);
 
     expect(res.headers).toHaveProperty(
@@ -35,7 +33,6 @@ describe('List Group Criterias', () => {
       'application/json; charset=utf-8'
     );
     expect(res.status).toBe(401);
-
     expect(validate.jsonSchema(res.body, expiredTokenSchema)).toBe(true);
   });
 });
