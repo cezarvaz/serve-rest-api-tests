61b3d7f007d08271dc54e576277200f5794877df
Jaime Furlan <jaime.rosa@solides.com.br>
2023-08-17 10:46:52 -0300

    Implementado validação de objeto e schama no get

--- schemas/rates/get-rates.js
+++ schemas/rates/get-rates.js
@@ -2,51 +2,68 @@ const get_rates = {
   $schema: 'http://json-schema.org/draft-07/schema',
   $id: '#success-get-rates',
   type: 'object',
-  required: ['data', 'included'],
+  required: ['data', 'included', 'meta'],
   additionalProperties: false,
   properties: {
     data: {
-      type: 'object',
-      required: ['id', 'type', 'attributes'],
-      additionalProperties: false,
-      properties: {
-        id: {
-          type: 'integer',
-        },
-        type: {
-          type: 'string',
-        },
-        attributes: {
-          type: 'object',
-          required: [
-            'name',
-            'external_id',
-            'archived',
-            'axis',
-            'created_at',
-            'updated_at',
-          ],
-          additionalProperties: false,
-          properties: {
-            name: {
-              type: 'string',
-            },
-            external_id: {
-              type: ['null', 'integer'],
-            },
-            archived: {
-              type: 'boolean',
-            },
-            axis: {
-              type: ['string', 'null'],
-            },
-            created_at: {
-              type: 'string',
-              format: 'date-time',
-            },
-            updated_at: {
-              type: 'string',
-              format: 'date-time',
+      type: 'array',
+      items: {
+        type: 'object',
+        required: ['id', 'type', 'attributes'],
+        additionalProperties: false,
+        properties: {
+          id: {
+            type: 'number',
+          },
+          type: {
+            type: 'string',
+          },
+          attributes: {
+            type: 'object',
+            required: [
+              'company_id',
+              'evaluation_id',
+              'evaluator_id',
+              'evaluated_id',
+              'department_id',
+              'position_id',
+              'skill_id',
+              'rate',
+              'factor_at_moment',
+              'skill_group_id',
+            ],
+            additionalProperties: false,
+            properties: {
+              company_id: {
+                type: 'number',
+              },
+              evaluation_id: {
+                type: 'number',
+              },
+              evaluator_id: {
+                type: 'number',
+              },
+              evaluated_id: {
+                type: 'number',
+              },
+              department_id: {
+                type: 'number',
+              },
+              position_id: {
+                type: 'number',
+              },
+              skill_id: {
+                type: 'number',
+              },
+              rate: {
+                type: 'number',
+              },
+              factor_at_moment: {
+                type: 'number',
+              },
+              skill_group_id: {
+                type: 'number',
+              },
             },
           },
         },
@@ -55,6 +72,45 @@ const get_rates = {
     included: {
       type: 'array',
       items: {},
+    },
+    meta: {
+      type: 'object',
+      required: ['pagination'],
+      additionalProperties: false,
+      properties: {
+        pagination: {
+          type: 'object',
+          required: [
+            'current',
+            'previous',
+            'next',
+            'per_page',
+            'pages',
+            'total_count',
+          ],
+          additionalProperties: false,
+          properties: {
+            current: {
+              type: 'number',
+            },
+            previous: {
+              type: 'null',
+            },
+            next: {
+              type: 'null',
+            },
+            per_page: {
+              type: 'number',
+            },
+            pages: {
+              type: 'number',
+            },
+            total_count: {
+              type: 'number',
+            },
+          },
+        },
+      },
     },
   },
 };
