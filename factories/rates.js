6e3a1b25849adee298a2f223176ed0ad4c5940b2
Jaime Furlan <jaime.rosa@solides.com.br>
2023-08-17 11:50:07 -0300

    test post-rates

--- factories/rates.js
+++ factories/rates.js
@@ -1,36 +1,36 @@
 import request from 'config/request';
 import client from 'helpers/AuthClient';
-// import fakerBr from 'faker-br';
+import fakerBr from 'faker-br';
 
 beforeAll(async () => {
   await client.auth();
 });
 
 class rates {
-  //   async create() {
-  //     const payload = {
-  //       solicitation: {
-  //         name: `Rates${fakerBr.random.number({
-  //           max: 999999999999,
-  //         })}_criada pela automação de testes de API`,
-  //         description: fakerBr.random.words(),
-  //         started_at: '2029-11-21',
-  //         finished_at: '2029-12-29',
-  //       },
-  //     };
+  async create(evaluationId) {
+    const payload = {
+      rate: {
+        rates_attributes: [
+          { rate: fakerBr.random.number({ min: 1, max: 5 }), skill_id: 8051 },
+          { rate: fakerBr.random.number({ min: 1, max: 5 }), skill_id: 8418 },
+          { rate: fakerBr.random.number({ min: 1, max: 5 }), skill_id: 8799 },
+        ],
+        comments_attributes: {
+          text: '<p>Teste API</p>',
+        },
+      },
+    };
 
-  //     const { body } = await request
-  //       .post('rates')
-  //       .set('Authorization', `Bearer ${client.accessToken}`)
-  //       .send(payload)
-  //       .expect(201);
+    const { body } = await request
+      .post(`evaluations/${evaluationId}/rates`)
+      .set('Authorization', `Bearer ${client.accessToken}`)
+      .send(payload)
+      .expect(201);
 
-  //     this.id = body.data.id;
-  //     this.name = body.data.attributes.name;
-  //     this.description = body.data.attributes.description;
-  //     this.started_at = body.data.attributes.started_at;
-  //     this.finished_at = body.data.attributes.finished_at;
-  //   }
+    this.rate_1 = body.data[0].attributes.rate;
+    this.rate_2 = body.data[1].attributes.rate;
+    this.rate_3 = body.data[2].attributes.rate;
+  }
 
   async getLastItem(solicitation_id) {
     const { body } = await request
