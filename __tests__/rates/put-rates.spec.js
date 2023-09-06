a9b89e36a95a0b7cb7e4bf612853cbf86ea4f813
Jaime Furlan <jaime.rosa@solides.com.br>
2023-09-06 12:39:14 -0300

    Ajuste rates

--- __tests__/rates/put-rates.spec.js
+++ __tests__/rates/put-rates.spec.js
@@ -17,7 +17,7 @@ let payload;
 describe('put rates', () => {
   beforeAll(async () => {
     await client.auth();
-    await Solicitations.getLastItem(3);
+    await Solicitations.getLastItem(0);
     await SolicitationsEvaluations.create(Solicitations.lastId);
     await Rates.getLastItem(Solicitations.lastId);
 
